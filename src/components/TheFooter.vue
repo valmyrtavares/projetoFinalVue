4e4e5c903322a291e6c7cd4fe4019be426ce361b
Valmyr Tavares <valmyr.lima@millennium.com.br>
2020-04-06 17:06:20 -0300

    Aula 902 Foi feito a criação do footer e do header por enquato o assunto aqui é compomente muita estilização e pouco vue

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,28 @@
+<template>
+<footer>
+<p>Raneks alguns direitos reservados</p>
+</footer>
+ 
+</template>
+
+<script>
+export default {
+name:"TheFooter"
+}
+</script>
+
+<style>
+footer{
+    background:#87f;
+    padding:25px;
+    text-align:center;
+    font-size:1.2rem;
+    color:white;
+    position: relative;
+    top: 75px;
+
+
+}
+
+
+</style>
